eb8f15a0eff92caca276d9d23cf37bb82f581b86
samar <samareeyassin155@gmail.com>
2021-06-09 18:59:41 +0200

    lineclipping

--- GrProject/GrProject.cpp
+++ GrProject/GrProject.cpp
@@ -465,11 +465,126 @@ public:
 
             SetPixel(hdc, x, y, color);
     }
+};
 
 
+union OutCode
+{
+    unsigned All : 4;
+    struct
+    {
+        unsigned left : 1, top : 1, right : 1, bottom : 1;
+    };
+};
+OutCode GetOutCode(double x, double y, int xleft, int ytop, int xright, int ybottom)
+{
+    OutCode out;
+    out.All = 0;
+    if (x < xleft)
+        out.left = 1;
+    else if (x > xright)
+        out.right = 1;
+    if (y < ytop)
+        out.top = 1;
+    else if (y > ybottom)
+        out.bottom = 1;
+    return out;
+}
 
+class lineClipping
+{
+public:
+    int counter = 0;
+    Point p1;
+    Point p2;
+    Point p3;
+    Point p4;
+
+    void lbutton(LPARAM lParam, HWND hwnd) {
+        HDC hdc = GetDC(hwnd);
+        if (counter == 0) {
+            p1.x = LOWORD(lParam);
+            p1.y = HIWORD(lParam);
+            counter++;
+        }
+        else if (counter == 1) {
+            p2.x = LOWORD(lParam);
+            p2.y = HIWORD(lParam);
+            Rectangle(hdc, p1.x, p1.y, p2.x, p2.y);
+            counter++;
+        }
+        else if (counter == 2) {
+            p3.x = LOWORD(lParam);
+            p3.y = HIWORD(lParam);
+            counter++;
+        }
+        else if (counter == 3) {
+            p4.x = LOWORD(lParam);
+            p4.y = HIWORD(lParam);
+            CohenSuth(hdc, p3.x, p3.y, p4.x, p4.y, p1.x, p1.y, p2.x, p2.y);
+            counter = 0;
+
+        }
+    }
+
+    void VIntersect(double xs, double ys, double xe, double ye, int x, double* xi, double* yi)
+    {
+        *xi = x;
+        *yi = ys + (x - xs) * (ye - ys) / (xe - xs);
+    }
+    void HIntersect(double xs, double ys, double xe, double ye, int y, double* xi, double* yi)
+    {
+        *yi = y;
+        *xi = xs + (y - ys) * (xe - xs) / (ye - ys);
+    }
+    void CohenSuth(HDC hdc, int xs, int ys, int xe, int ye, int xleft, int ytop, int xright, int ybottom)
+    {
+        double x1 = xs, y1 = ys, x2 = xe, y2 = ye;
+        OutCode out1 = GetOutCode(x1, y1, xleft, ytop, xright, ybottom);
+        OutCode out2 = GetOutCode(x2, y2, xleft, ytop, xright, ybottom);
+        while ((out1.All || out2.All) && !(out1.All & out2.All))
+        {
+            double xi, yi;
+            if (out1.All)
+            {
+                if (out1.left)
+                    VIntersect(x1, y1, x2, y2, xleft, &xi, &yi);
+                else if (out1.top)
+                    HIntersect(x1, y1, x2, y2, ytop, &xi, &yi);
+                else if (out1.right)
+                    VIntersect(x1, y1, x2, y2, xright, &xi, &yi);
+                else
+                    HIntersect(x1, y1, x2, y2, ybottom, &xi, &yi);
+                x1 = xi;
+                y1 = yi;
+                out1 = GetOutCode(x1, y1, xleft, ytop, xright, ybottom);
+            }
+            else
+            {
+                if (out2.left)
+                    VIntersect(x1, y1, x2, y2, xleft, &xi, &yi);
+                else if (out2.top)
+                    HIntersect(x1, y1, x2, y2, ytop, &xi, &yi);
+                else if (out2.right)
+                    VIntersect(x1, y1, x2, y2, xright, &xi, &yi);
+                else
+                    HIntersect(x1, y1, x2, y2, ybottom, &xi, &yi);
+                x2 = xi;
+                y2 = yi;
+                out2 = GetOutCode(x2, y2, xleft, ytop, xright, ybottom);
+            }
+        }
+        if (!out1.All && !out2.All)
+        {
+
+            MoveToEx(hdc, round(x1), round(y1), NULL);
+            LineTo(hdc, round(x2), round(y2));
+        }
+    }
 
 };
+
+
 
 /*  Declare Windows procedure  */
 LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
@@ -551,6 +666,7 @@ ParametricLine parLine;
 MidpointLine midpoLine;
 DDALine dLine;
 PointClipping pointclipping;
+lineClipping lineclipping;
 
 int flag = 0;
 int var;
@@ -606,6 +722,10 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM 
                 pointclipping.lbutton(lParam, hwnd);
                 break;
             }
+            case 12: {
+                lineclipping.lbutton(lParam, hwnd);
+                break;
+            }
 
         }
         break;
@@ -654,7 +774,7 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM 
             flag = 11;
             break;
         case LineClipping_ID:
-            flag = 11;
+            flag = 12;
             break;
         case Black_ID:
 			color = RGB(0, 0, 0);
